f62fd0286cf9e16b3a9a4203394e82a9a411542c
vivafree <122075718+vivafree@users.noreply.github.com>
2024-06-05 11:42:45 +0900

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,24 @@
+let slideIndex = 0;
+showSlides(slideIndex);
+
+function showSlides(index) {
+    let slides = document.getElementsByClassName('slide');
+    if (index >= slides.length) {
+        slideIndex = 0;
+    }
+    if (index < 0) {
+        slideIndex = slides.length - 1;
+    }
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = 'none';
+    }
+    slides[slideIndex].style.display = 'block';
+}
+
+function nextSlide() {
+    showSlides(++slideIndex);
+}
+
+function prevSlide() {
+    showSlides(--slideIndex);
+}
